20f4f7a42ceedf045dfa3fbf027ab342f12e2a92
Lars Beyer <beyer_l@t-online.de>
2015-05-19 11:45:27 +0200

    Initial commit

---
+++ src/main/java/de/unimuenster/wi/wfm/application/RentalAgreementApplication.java
@@ -0,0 +1,10 @@
+package de.unimuenster.wi.wfm.application;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+
+@ProcessApplication("Loan Approval App")
+public class RentalAgreementApplication extends ServletProcessApplication {
+  // empty implementation
+}
