65a192b8c8a74b52d0bb636f68d1fbffd954f21b
Lars Beyer <beyer_l@t-online.de>
2015-06-04 15:55:05 +0200

    split project into two separate ones

---
+++ capitol/src/main/java/de/unimuenster/wi/wfm/application/RentalAgreementApplication.java
@@ -0,0 +1,10 @@
+package de.unimuenster.wi.wfm.application;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+
+@ProcessApplication("Rental Agreement Application")
+public class RentalAgreementApplication extends ServletProcessApplication {
+  // empty implementation
+}
