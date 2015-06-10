d9c7d13ea26751d7158c27c08d4f80c27817d491
Lars Beyer <beyer_l@t-online.de>
2015-06-10 17:35:01 +0200

    Made capitol-rentail-agreement deployable

---
+++ capitol/src/main/java/de/unimuenster/wi/wfm/delegates/CloseCaseDelegate.java
@@ -0,0 +1,14 @@
+package de.unimuenster.wi.wfm.delegates;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+ 
+public class CloseCaseDelegate implements JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
