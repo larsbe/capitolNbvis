955a7b5f561d1b0e059578639eef2bfbb89fe83f
Johannes- <j_vosc01@uni-muenster.de>
2015-06-10 18:05:59 +0200

    Made capitol-liability-case deployable

---
+++ capitol/src/main/java/de/unimuenster/wi/wfm/delegates/CreateNewLiabilityCaseDelegate.java
@@ -0,0 +1,15 @@
+package de.unimuenster.wi.wfm.delegates;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class CreateNewLiabilityCaseDelegate implements JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution arg0) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
---
+++ capitol/src/main/java/de/unimuenster/wi/wfm/delegates/InformBvisAboutRejectionDelegate.java
@@ -0,0 +1,14 @@
+package de.unimuenster.wi.wfm.delegates;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class InformBvisAboutRejectionDelegate  implements JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution arg0) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
