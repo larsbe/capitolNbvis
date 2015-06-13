c6df2304b7242934157b94cec12769f2d6b0da92
Jonas Gerlach <j.gerlach@uni-muenster.de>
2015-06-13 04:13:38 +0200

    first msg test + imges

--- capitol/src/main/java/de/unimuenster/wi/wfm/delegates/GatherAndPrepareContractDelegate.java
+++ capitol/src/main/java/de/unimuenster/wi/wfm/delegates/GatherAndPrepareContractDelegate.java
@@ -5,7 +5,7 @@ import org.camunda.bpm.engine.delegate.JavaDelegate;
 
 public class GatherAndPrepareContractDelegate implements JavaDelegate {
 
-	@Override
+	
 	public void execute(DelegateExecution arg0) throws Exception {
 		// TODO Auto-generated method stub
 
---
+++ capitol2bvis/src/main/java/de/unimuenster/wi/wfm/sharedLib/data/Image.java
@@ -0,0 +1,21 @@
+package de.unimuenster.wi.wfm.sharedLib.data;
+
+public class Image {
+	
+	private String url;
+	private String description;
+	
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+		
+}
