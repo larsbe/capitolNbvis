c6eecf7128145b3aa755c9f1505c6e72fa3fc576
Jonas Gerlach <j.gerlach@uni-muenster.de>
2015-07-01 10:42:28 +0200

    Customer Web App

--- capitol/src/main/java/de/unimuenster/wi/wfm/util/Constants.java
+++ capitol/src/main/java/de/unimuenster/wi/wfm/util/Constants.java
@@ -5,4 +5,6 @@ public class Constants {
 	public static String COMPANY_NAME = "Capitol for People Inc.";
 	public static String IMG_METADATA_LIABILITYCLAIM_ABBR = "CASE";
 	
+	public static String IMAGE_SERVICE_URL = "http://capitol.jonasgerlach.de/capitolImageService/AddMetadataToImage.php";
+	
 }
