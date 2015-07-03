be7466b4a8e10a1b0dccd94e2dfd9876632d870b
Johannes- <j_vosc01@uni-muenster.de>
2015-07-03 18:48:57 +0200

    Further Implementation

--- capitol/src/main/java/de/unimuenster/wi/wfm/web/CheckIfAdjestmentIsNeeded.java
+++ capitol/src/main/java/de/unimuenster/wi/wfm/web/CheckIfAdjestmentIsNeeded.java
@@ -1,6 +1,8 @@
 package de.unimuenster.wi.wfm.web;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.Collection;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.ConversationScoped;
@@ -8,6 +10,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.camunda.bpm.engine.cdi.BusinessProcess;
+import org.camunda.bpm.engine.cdi.jsf.TaskForm;
 
 import de.unimuenster.wi.wfm.ejb.LiabilityCaseServiceBean;
 import de.unimuenster.wi.wfm.entitiy.LiabilityCase;
@@ -21,10 +24,24 @@ public class CheckIfAdjestmentIsNeeded implements Serializable {
 	@Inject
 	private BusinessProcess businessProcess;
 	
+	// Inject task form available through the camunda cdi artifact
+	@Inject
+	private TaskForm taskForm;
+	
 	@EJB
 	private LiabilityCaseServiceBean liabilityCaseService;
 	private LiabilityCase liabilityCase;
 	private long liabilityCaseId;
+	
+	protected boolean adjustmentNeeded = false;
+	
+	public boolean getAdjustmentNeeded() {
+		return adjustmentNeeded;
+	}
+	
+	public void setAdjustmentNeeded(boolean adjustmentNeeded) {
+		this.adjustmentNeeded = adjustmentNeeded;
+	}
 	
 	public long getLiabilityCaseId() {
 		liabilityCaseId = (Long) businessProcess.getVariable("caseID");
@@ -37,5 +54,20 @@ public class CheckIfAdjestmentIsNeeded implements Serializable {
 		}
 		return liabilityCase;
 	}
+	
+	public void submitForm() throws IOException {
+	    // Persist updated order entity and complete task form
+		businessProcess.setVariable("adjustmentIsNeeded",getAdjustmentNeeded());
+		try {
+			// Complete user task from
+			//Doesn't work so far!!!!!!!!!!!!!!!!!!!!
+			taskForm.completeTask();
+		} catch (IOException e) {
+			// Rollback both transactions on error
+			throw new RuntimeException("Cannot complete task", e);
+		}
+		
+		
+	  }
 	
 }
