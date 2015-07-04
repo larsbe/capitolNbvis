b25f6848b49ee3d526ecd9d5db0f172aa0921eeb
Johannes- <j_vosc01@uni-muenster.de>
2015-07-04 21:28:36 +0200

    Something

--- capitol2bvis/src/main/java/de/unimuenster/wi/wfm/sharedLib/data/CarData.java
+++ capitol2bvis/src/main/java/de/unimuenster/wi/wfm/sharedLib/data/CarData.java
@@ -1,44 +1,28 @@
 package de.unimuenster.wi.wfm.sharedLib.data;
 
-import java.math.BigDecimal;
-import java.util.List;
-
 public class CarData {
 
-	private String hsntsn;
+	private String hsn;
+	private String tsn;
 	private String amount;
-	private InsuranceType insuranceType;
-	private List<InsuranceBenefit> benefits;
-	private BigDecimal insurancePrice;
 	
-	public String getHsntsn() {
-		return hsntsn;
+	public String getHsn() {
+		return hsn;
 	}
-	public void setHsntsn(String hsntsn) {
-		this.hsntsn = hsntsn;
+	public void setHsn(String hsn) {
+		this.hsn = hsn;
 	}
+	public String getTsn() {
+		return tsn;
+	}
+	public void setTsn(String tsn) {
+		this.tsn = tsn;
+	}
+	
 	public String getAmount() {
 		return amount;
 	}
 	public void setAmount(String amount) {
 		this.amount = amount;
-	}
-	public InsuranceType getInsuranceType() {
-		return insuranceType;
-	}
-	public void setInsuranceType(InsuranceType insuranceType) {
-		this.insuranceType = insuranceType;
-	}
-	public List<InsuranceBenefit> getBenefits() {
-		return benefits;
-	}
-	public void setBenefits(List<InsuranceBenefit> benefits) {
-		this.benefits = benefits;
-	}
-	public BigDecimal getInsurancePrice() {
-		return insurancePrice;
-	}
-	public void setInsurancePrice(BigDecimal insurancePrice) {
-		this.insurancePrice = insurancePrice;
 	}
 }
