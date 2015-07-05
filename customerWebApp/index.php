239b2d112b11522f2265e433246da93052966a84
Robert H <robert.heidekrueger@uni-muenster.de>
2015-07-05 23:46:46 +0200

    make webapp home screen compatible

--- customerWebApp/index.php
+++ customerWebApp/index.php
@@ -32,10 +32,14 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimal-ui" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="yes" />
     <!--<link rel="icon" href="../../favicon.ico">-->
 
     <title>BVIS Customer WebApp</title>
 
+    <link rel="apple-touch-icon" href="assets/img/touch-icon.png">
     <!-- Bootstrap core CSS -->
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/css/bootstrap-theme.css" rel="stylesheet">
@@ -69,9 +73,9 @@
       <div class="header clearfix">
         <nav>
           <ul class="nav nav-pills pull-right">
-            <li role="presentation" class="<?php if($_GET['page'] == 'request') echo 'active'; ?>"><a href="index.php?page=request">Request Offer</a></li>
-            <li role="presentation" class="<?php if($_GET['page'] == 'fileupload') echo 'active'; ?>"><a href="index.php?page=fileupload">Contract Upload</a></li>
-            <li role="presentation" class="<?php if($_GET['page'] == 'liability') echo 'active'; ?>"><a href="index.php?page=liability">New Insurance Claim</a></li>
+            <li role="presentation" class="<?php if($_GET['page'] == 'request') echo 'active'; ?>"><a href="index.php?page=request" target="_self">Request Offer</a></li>
+            <li role="presentation" class="<?php if($_GET['page'] == 'fileupload') echo 'active'; ?>"><a href="index.php?page=fileupload" target="_self">Contract Upload</a></li>
+            <li role="presentation" class="<?php if($_GET['page'] == 'liability') echo 'active'; ?>"><a href="index.php?page=liability" target="_self">New Insurance Claim</a></li>
           </ul>
         </nav>
         <!--<h3 class="text-muted">BVIS</h3>-->
@@ -87,6 +91,17 @@
     </div> <!-- /container -->
 
 
-
+    <script>
+    // prevent homescreen app from opening links in external browser
+    var a=document.getElementsByTagName("a");
+    for(var i=0;i<a.length;i++) {
+        if(!a[i].onclick && a[i].getAttribute("target") != "_blank") {
+            a[i].onclick=function() {
+                    window.location=this.getAttribute("href");
+                    return false; 
+            }
+        }
+    }
+    </script>
   </body>
 </html>
