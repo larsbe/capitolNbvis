cbc281329ff0a5e00c2d5e4ce61be3eb63c7d5e9
djauernig <d_jaue01@uni-muenster.de>
2015-07-05 23:54:20 +0200

    BVIS - Message Names

--- capitol2bvis/src/main/java/de/unimuenster/wi/wfm/sharedLib/constants/Bvis.java
+++ capitol2bvis/src/main/java/de/unimuenster/wi/wfm/sharedLib/constants/Bvis.java
@@ -1,5 +1,23 @@
 package de.unimuenster.wi.wfm.sharedLib.constants;
 
 public class Bvis {
-
+	
+	// <<<--- rental agreement --->>>
+	
+	// WebApp --> BVIS
+	public static final String REST_MSG_NEW_RENTAL_AGREEMENT_REQUEST = "NewRentalAgreementRequest";
+	public static final String REST_MSG_RENTAL_AGREEMENT_CONTRACT = "RentalAgreementContract";
+	
+	// Capitol --> BVIS
+	public static final String REST_MSG_AGREEMENT_CONDITIONS_WITH_INSURANCE_BENEFITS = "AgreementConditionsWithInsurance";
+	
+	
+	
+	// <<<--- liability case --->>>
+	
+	// WebApp --> BVIS
+	public static final String REST_MSG_NEW_LIABILITY_CASE = "newLiabilityCase";
+	
+	// Capitol --> BVIS
+	public static final String REST_MSG_CLAIM_INFORMATION = "ClaimInformation";
 }
