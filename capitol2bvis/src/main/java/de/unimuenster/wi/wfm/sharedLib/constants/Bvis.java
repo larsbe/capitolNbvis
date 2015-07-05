436f250ed4de91b47da55aa24236955df2d11f0b
Jonas Gerlach <j.gerlach@uni-muenster.de>
2015-07-05 11:26:57 +0200

    Shared MSG Names

---
+++ capitol2bvis/src/main/java/de/unimuenster/wi/wfm/sharedLib/constants/Bvis.java
@@ -0,0 +1,5 @@
+package de.unimuenster.wi.wfm.sharedLib.constants;
+
+public class Bvis {
+
+}
---
+++ capitol2bvis/src/main/java/de/unimuenster/wi/wfm/sharedLib/constants/Capitol.java
@@ -0,0 +1,7 @@
+package de.unimuenster.wi.wfm.sharedLib.constants;
+
+public class Capitol {
+
+	public static final String REST_MSG_NEW_LIABILITY_CASE = "newLiabilityCase";
+	
+}
