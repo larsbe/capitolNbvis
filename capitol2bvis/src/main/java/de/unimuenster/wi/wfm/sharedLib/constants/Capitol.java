c8ddaa9a815d78894f3d47dc3d077d4c2f10b5a1
Johannes- <j_vosc01@uni-muenster.de>
2015-07-05 23:44:00 +0200

    Add missing Massages

--- capitol2bvis/src/main/java/de/unimuenster/wi/wfm/sharedLib/constants/Capitol.java
+++ capitol2bvis/src/main/java/de/unimuenster/wi/wfm/sharedLib/constants/Capitol.java
@@ -5,4 +5,6 @@ public class Capitol {
 	public static final String REST_MSG_NEW_LIABILITY_CASE = "newLiabilityCase";
 	public static final String REST_MSG_NEW_AGREEMENT_CONDITIONS = "newAgreementConditions";
 	public static final String REST_MSG_NEW_STANDARD_RENTAL_AGREEMENT = "newStandardRentalAgreement";
+	public static final String REST_MSG_REVISED_AGREEMENTL_CONDITIONS = "revisedAgreementConditions";
+	public static final String REST_MSG_CONTRACT_STATUS = "contractStatus ";
 }
