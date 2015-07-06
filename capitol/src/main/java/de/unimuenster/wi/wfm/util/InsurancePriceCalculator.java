fd83a5e340742e6111ecf2c2396b4479b0e3008e
Johannes- <j_vosc01@uni-muenster.de>
2015-07-06 14:17:46 +0200

    Finished PriceCalculator

--- capitol/src/main/java/de/unimuenster/wi/wfm/util/InsurancePriceCalculator.java
+++ capitol/src/main/java/de/unimuenster/wi/wfm/util/InsurancePriceCalculator.java
@@ -4,22 +4,25 @@ import java.math.BigDecimal;
 import java.util.Collection;
 
 import de.unimuenster.wi.wfm.entitiy.CarInformation;
+import de.unimuenster.wi.wfm.entitiy.IndividualInsuranceBenefitEntity;
 import de.unimuenster.wi.wfm.sharedLib.data.InsuranceBenefit;
 import de.unimuenster.wi.wfm.sharedLib.data.InsuranceType;
 
 public class InsurancePriceCalculator {
 
-	public BigDecimal calculateInsurancePrice(String hsn, String tsn, String year, Collection<InsuranceBenefit> insuranceBenefits, InsuranceType insuranceType){
+	public BigDecimal calculateInsurancePrice(String hsn, String tsn, String year, Collection<InsuranceBenefit> insuranceBenefits, Collection<IndividualInsuranceBenefitEntity> individualInsuranceBenefitEntitys, InsuranceType insuranceType){
 		BigDecimal price;
 		BigDecimal priceDependentOfCar;
 		BigDecimal priceOfBenefits;
+		BigDecimal priceOfIndividualBenefits;
 		CarInformation carinformation;
 		carinformation = CarInformationService.GetCarInformation(hsn, tsn, year);
 		
 		priceDependentOfCar= BigDecimal.valueOf(getClassificationForInsuranceType(insuranceType,carinformation)).multiply(BigDecimal.valueOf(10.21));
 		priceOfBenefits=getSumOfAllInsuranceBenefits(insuranceBenefits);
+		priceOfIndividualBenefits=getSumOfAllIndividualInsuranceBenefits(individualInsuranceBenefitEntitys);
 		
-		price = insuranceType.toPrice().add(priceDependentOfCar).add(priceOfBenefits);
+		price = insuranceType.toPrice().add(priceDependentOfCar).add(priceOfBenefits).add(priceOfIndividualBenefits);
 		return price;
 	}
 	
@@ -37,6 +40,15 @@ public class InsurancePriceCalculator {
 		sum = BigDecimal.ZERO;
 		for(InsuranceBenefit insuranceBenefit : insuranceBenefits){
 			sum = sum.add(insuranceBenefit.toPrice());
+		}
+		return sum;
+	}
+	
+	public BigDecimal getSumOfAllIndividualInsuranceBenefits(Collection<IndividualInsuranceBenefitEntity> individualInsuranceBenefitEntitys){
+		BigDecimal sum;
+		sum = BigDecimal.ZERO;
+		for(IndividualInsuranceBenefitEntity insuranceBenefit : individualInsuranceBenefitEntitys){
+			sum = sum.add(insuranceBenefit.getPrice());
 		}
 		return sum;
 	}
