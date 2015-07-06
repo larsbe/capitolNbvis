524f44675b02c609383005256ee43ae0c64d9398
Jonas Gerlach <j.gerlach@uni-muenster.de>
2015-07-06 21:46:09 +0200

    fix

--- CapitolSandbox/src/main/java/SandboxJonas/Sandbox.java
+++ CapitolSandbox/src/main/java/SandboxJonas/Sandbox.java
@@ -13,9 +13,29 @@ public class Sandbox {
 
 	public static void main(String[] args) {
 
-		System.out.println(CapitolREST.ContractStatus(3L, true));
+		RentalAgreementMessage message = new RentalAgreementMessage();
+		message.setRentalAgreementRequestId(188);
+		message.setAdditionalInfo("Test");
+		message.setBenefits(Arrays.asList(InsuranceBenefit.BRAKEDOWNCOVER));
+		message.setInsuranceType(InsuranceType.PARTIAL);
+		CustomerData customerData = new CustomerData();
+		customerData.setName("Robert Voscort");
+		customerData.setAddress("TestAddress");
+		customerData.setCompany("Capitol");
+		customerData.setEmail("robert.voscort@capitol.to");
+		customerData.setPhoneNumber("0190666666");
+		message.setCustomerData(customerData);
+		de.unimuenster.wi.wfm.sharedLib.data.CarData car1 = new de.unimuenster.wi.wfm.sharedLib.data.CarData();
+		car1.setLicenseNumber("TR-L-2608");
+		car1.setHsn("489032");
+		car1.setTsn("8593402");
+		de.unimuenster.wi.wfm.sharedLib.data.CarData car2 = new de.unimuenster.wi.wfm.sharedLib.data.CarData();
+		car2.setLicenseNumber("MS-X-320");
+		car2.setHsn("49302");
+		car2.setTsn("520358");
+		message.setCarsData(Arrays.asList(car1, car2));
 
-		RestHelper.SendMessage("http://localhost:8080/engine-rest/engine/default/message", CapitolREST.ContractStatus(3L, true));
+		RestHelper.SendMessage("http://localhost:8080/engine-rest/engine/default/message", CapitolREST.NewStandardRentalAgreement(message));
 	}
 
 }
