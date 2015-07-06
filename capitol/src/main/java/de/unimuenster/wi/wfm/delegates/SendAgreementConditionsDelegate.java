50eb071942ffe18050016e9b2fad5cf6a31e9318
Lars Beyer <beyer_l@t-online.de>
2015-07-06 21:18:31 +0200

    Implemented last two service tasks

--- capitol/src/main/java/de/unimuenster/wi/wfm/delegates/SendAgreementConditionsDelegate.java
+++ capitol/src/main/java/de/unimuenster/wi/wfm/delegates/SendAgreementConditionsDelegate.java
@@ -1,8 +1,11 @@
 package de.unimuenster.wi.wfm.delegates;
 
+import javax.inject.Named;
+
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
+@Named
 public class SendAgreementConditionsDelegate implements JavaDelegate {
 
 	
