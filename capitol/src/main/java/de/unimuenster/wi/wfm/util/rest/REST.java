b12b6c5553d26852d0fa309d3fb94b428ffa0278
Jonas Gerlach <j.gerlach@uni-muenster.de>
2015-07-06 17:31:35 +0200

    Helper

---
+++ capitol/src/main/java/de/unimuenster/wi/wfm/util/rest/REST.java
@@ -0,0 +1,26 @@
+package de.unimuenster.wi.wfm.util.rest;
+
+import de.unimuenster.wi.wfm.entitiy.LiabilityCase;
+import de.unimuenster.wi.wfm.sharedLib.rest.BvisREST;
+import de.unimuenster.wi.wfm.sharedLib.rest.RestHelper;
+import de.unimuenster.wi.wfm.util.Constants;
+
+public class REST extends RestHelper {
+
+	public static void SendLiabilityCaseRejectionInformation(LiabilityCase claim) {
+		String msg = BvisREST.LiabilityCaseRejectionInformation(
+				"correlationKey", claim.getReportUrl());
+		SendMessageToBVIS(msg);
+	}
+
+	public static void SendLiabilityCasePaymentInformation(LiabilityCase claim) {
+		String msg = BvisREST.LiabilityCasePaymentInformation("correlationKey",
+				claim.getReportUrl(), claim.getInsuranceSum());
+		SendMessageToBVIS(msg);
+	}
+
+	private static void SendMessageToBVIS(String msg) {
+		RestHelper.SendMessage(Constants.BVIS_CAMUNDA_REST, msg);
+	}
+
+}
