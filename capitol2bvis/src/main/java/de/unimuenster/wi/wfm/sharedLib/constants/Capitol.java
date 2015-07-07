bdaa02eca2b8f89680eaf0d25deb0063948dec5b
Jonas Gerlach <j.gerlach@uni-muenster.de>
2015-07-07 07:48:03 +0200

    Lesezeichen LARS :(

--- capitol2bvis/src/main/java/de/unimuenster/wi/wfm/sharedLib/constants/Capitol.java
+++ capitol2bvis/src/main/java/de/unimuenster/wi/wfm/sharedLib/constants/Capitol.java
@@ -6,5 +6,5 @@ public class Capitol {
 	public static final String REST_MSG_NEW_AGREEMENT_CONDITIONS = "newAgreementConditions";
 	public static final String REST_MSG_NEW_STANDARD_RENTAL_AGREEMENT = "newStandardRentalAgreement";
 	public static final String REST_MSG_REVISED_AGREEMENTL_CONDITIONS = "revisedAgreementConditions";
-	public static final String REST_MSG_CONTRACT_STATUS = "contractStatus ";
+	public static final String REST_MSG_CONTRACT_STATUS = "contractStatus";
 }
